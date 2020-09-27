48475746606882c4b2e25e6a488b876fe9cc17c3
Shubham Kumar <shubhamkumar1920>
2020-09-27 20:14:22 +0530

    create homepage

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+// assets
+app.use(express.static('public'))
+// set Template engine 
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine','ejs')
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
