3d014cbfff9e2112b87926f75c19a322ae5afb80
Shubham Kumar <shubhamkumar1920>
2020-09-30 22:36:22 +0530

    authenticae sysytm

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 const homeController = require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
+const guest = require('../app/http/middleware/guest')
 
 function initRoutes(app) {
     app.get('/',homeController().index)
@@ -9,9 +10,17 @@ function initRoutes(app) {
     
     app.post('/update-cart',cartController().update)
 
-    app.get('/login',authController().login)
+    app.get('/login',guest,authController().login)
 
-    app.get('/register',authController().register)
+    app.post('/login',authController().postLogin)
+
+    app.get('/register',guest,authController().register)
+
+    app.get('/logout',guest,authController().logout)
+
+
+    app.post('/register',authController().postRegister)
+
 
 }
 module.exports = initRoutes
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 // Database connection
 const url = 'mongodb://localhost/pizza'
@@ -34,9 +35,16 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour 
 }))
 
+// Passport config 
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 // assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // Global middleware 
