7248aa36261f743be0460fb516f08a009da2b852
Shubham Kumar <shubhamkumar1920>
2020-09-30 19:54:41 +0530

    add-to-cart

---
+++ app/http/controllers/authController.js
@@ -0,0 +1,13 @@
+const { response } = require("express")
+
+function authController() {
+    return {
+        login(req, res) {
+            res.render('auth/login')
+        },
+        register(req, res) {
+            res.render('auth/register')
+        }
+    }
+}
+module.exports =authController
